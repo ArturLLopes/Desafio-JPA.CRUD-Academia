e4ab99f9b16d7e6bc8a60672d725d7112a0f0ce5
ArturLLopes <arturllopes@gmail.com>
2026-03-20 15:17:55 -0300

    FEAT: Implementação das anotações das entity

--- src/main/java/desafio/jpa/academia/academia_digital/entity/Matricula.java
+++ src/main/java/desafio/jpa/academia/academia_digital/entity/Matricula.java
@@ -1,13 +1,27 @@
 package desafio.jpa.academia.academia_digital.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_matricula")
 public class Matricula {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "aluno_id")
     private Aluno aluno;
 
     @JsonFormat(pattern = "dd/MM/yyyy")
